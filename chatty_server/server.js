389bf49cab1ec15dd6b0e785d4f9fbf6019809ac
gizemocak <gizem_ocak@outlook.com>
2019-06-04 23:15:22 +0000

    create a new server to implement web sockets

---
+++ chatty_server/server.js
@@ -0,0 +1,39 @@
+// server.js
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+
+// Set the port to 3001
+const PORT = 3001;
+
+// Create a new express server
+const server = express()
+  // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+// Create the WebSockets server
+const wss = new SocketServer({
+  server
+});
+
+// Set up a callback that will run when a client connects to the server
+// When a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  //sending data
+  ws.on('open', function open() {
+    ws.send('You are connected');
+  });
+
+  //receiving data
+  ws.on('message', function incoming(data) {
+    const obj = JSON.parse(data)
+    console.log(`User${obj.data.username} said ${obj.data.content}`);
+  });
+
+  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('close', () => console.log('Client disconnected'));
+});
