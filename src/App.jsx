a1e6cd07ab777aed582f40c635fcf0d1bc07bdaa
gizemocak <gizem_ocak@outlook.com>
2019-06-04 03:16:18 +0000

    pass props from app.js to child components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import ChatBar from "../src/ChatBar.jsx";
+import MessageList from "../src/MessageList.jsx";
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: "Bob",
+      messages: []
+    };
+  }
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <MessageList messages={this.state.messages} />
+        <ChatBar currentUser={this.state.currentUser} />
+      </div>
     );
   }
 }
