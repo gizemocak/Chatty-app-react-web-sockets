0d2293601e9adf15051e320b184ec1032b9415af
gizemocak <gizem_ocak@outlook.com>
2019-06-04 23:19:29 +0000

    connect to the new server

--- src/App.jsx
+++ src/App.jsx
@@ -25,31 +25,38 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {
-        id: 3,
-        username: "Michelle",
-        content: "Hello there!"
-      };
-      const messages = this.state.messages.concat(newMessage);
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({ messages: messages });
-    }, 3000);
+    // this is an "echo" websocket service for testing pusposes
+    this.connection = new WebSocket("ws://localhost:3001");
+    // listen to onmessage event
+    this.connection.onmessage = evt => {
+      console.log("meep", evt);
+      // add the new message to state
+      //   this.setState({
+      //   messages : this.state.messages.concat([ evt.data ])
+      // })
+    };
   }
 
   handleNewMessage = input => {
     const newMessage = {
       username: this.state.currentUser.name,
-      content: input,
-      id: this.state.messages.length + 1
+      content: input
+      //id: this.state.messages.length + 1
     };
     const oldMessages = this.state.messages;
     const newMessages = [...oldMessages, newMessage];
-    this.setState({ messages: newMessages });
+    // this.setState({ messages: newMessages });
+    this.sendDataToServer(newMessage);
+  };
+
+  sendDataToServer = data => {
+    var msg = {
+      type: "message",
+      data
+    };
+
+    // Send the msg object as a JSON-formatted string.
+    this.connection.send(JSON.stringify(msg));
   };
 
   render() {
