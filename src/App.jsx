ca862877a5f0dbba7093e438d19462f43e86d649
gizemocak <gizem_ocak@outlook.com>
2019-06-05 02:29:47 +0000

    delete the hardcoded messages from the app state

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,7 @@ class App extends Component {
 
     this.state = {
       currentUser: { name: "Bob" },
-      messages: [
-        {
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-          id: 1
-        },
-        {
-          username: "Anonymous",
-          content:
-            "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-          id: 2
-        }
-      ]
+      messages: []
     };
   }
 
