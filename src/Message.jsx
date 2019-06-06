91a0664371a85032c69c55b88651888976bf5635
gizemocak <gizem_ocak@outlook.com>
2019-06-06 19:53:07 +0000

    Display images in chat

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
 
 class Message extends Component {
+  checkURL = url => {
+    return url.match(/\.(jpeg|jpg|gif|png)$/) != null;
+  };
+
   render() {
     const { message } = this.props;
+    const url = ["http", "png", "gif", "jpeg"];
 
     if (message.username === "__system__") {
       return <div className="message system">{message.content}</div>;
-    }
-    if (message.color) {
-      console.log("colooooor", message.color);
     }
     return (
       <div className="message">
         <span style={{ color: message.color }} className="message-username">
           {message.username}
         </span>
-        <span className="message-content">{message.content}</span>
+        {this.checkURL(message.content) ? (
+          <span className="message-content">
+            <img src={message.content} />
+          </span>
+        ) : (
+          <span className="message-content">{message.content}</span>
+        )}
       </div>
     );
   }
