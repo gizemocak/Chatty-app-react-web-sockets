c95947386dd09ee79d7cac81aa661fe20e26edc9
gizemocak <gizem_ocak@outlook.com>
2019-06-07 02:34:10 +0000

    users are able to post youtube videos

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,8 +1,61 @@
 import React, { Component } from "react";
 
 class Message extends Component {
-  checkURL = url => {
+  checkImageURL = url => {
     return url.match(/\.(jpeg|jpg|gif|png)$/) != null;
+  };
+
+  checkVideoURL = url => {
+    return url.match(/youtube/) != null;
+  };
+
+  getQueryFromVideoURL = url => {
+    console.log("this.getQueryFromVideoURL");
+    const splitted = url.split("watch?v=");
+    const query = splitted[splitted.length - 1].split("&");
+    return query[0];
+  };
+
+  embedYoutubeVideo = url => {
+    console.log("embed youtube video");
+    return (
+      <div
+        className="video"
+        style={{
+          position: "relative",
+          paddingBottom: "56.25%" /* 16:9 */,
+          paddingTop: 25,
+          height: 0,
+          zIndex: -1
+        }}
+      >
+        <iframe
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%"
+          }}
+          src={`https://www.youtube.com/embed/${this.getQueryFromVideoURL(
+            url
+          )}`}
+          frameBorder="0"
+        />
+      </div>
+    );
+  };
+
+  checkURls = url => {
+    const { message } = this.props;
+    console.log(this.checkVideoURL(url));
+    if (this.checkImageURL(url)) {
+      return <img src={message.content} />;
+    } else if (this.checkVideoURL(url)) {
+      return this.embedYoutubeVideo(url);
+    } else {
+      return message.content;
+    }
   };
 
   render() {
@@ -16,14 +69,9 @@ class Message extends Component {
         <span style={{ color: message.color }} className="message-username">
           {message.username}
         </span>
-        {this.checkURL(message.content) ? (
-          <span className="message-content">
-            <img src={message.content} />
-          </span>
-        ) : (
-          <span className="message-content">{message.content}</span>
-        )}
-        <hr />
+        <span className="message-content">
+          {this.checkURls(message.content)}
+        </span>
       </div>
     );
   }
